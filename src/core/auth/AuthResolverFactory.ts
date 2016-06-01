98612790675a50c6ab28c55ab4dff5b3b9faedf9
sergei_sergeev <sergeev.srg@gmail.com>
2016-06-01 22:38:37 +0300

    Removed redundant test. Added support for SharePoint Online China url.

--- src/core/auth/AuthResolverFactory.ts
+++ src/core/auth/AuthResolverFactory.ts
@@ -6,7 +6,8 @@ import {OnlineResolver} from './OnlineResolver';
 
 export class AuthResolverFactory {
   public resolve(siteUrl: string): IAuthResolver {
-    let isOnPrem: boolean = (url.parse(siteUrl)).host.indexOf('.sharepoint.com') === -1;
+    let host: string = (url.parse(siteUrl)).host;
+    let isOnPrem: boolean = host.indexOf('.sharepoint.com') === -1 && host.indexOf('.sharepoint.cn') === -1;
 
     if (isOnPrem) {
       return new OnPremResolver();
